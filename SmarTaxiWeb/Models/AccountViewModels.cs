be05f88028a191d6104408485be9da26de737900
Stephani <Stephani@STEPHANI>
2016-04-29 03:45:27 -0430

    Cambios en Vista Index y Loggin

--- SmarTaxiWeb/Models/AccountViewModels.cs
+++ SmarTaxiWeb/Models/AccountViewModels.cs
@@ -72,7 +72,7 @@ namespace SmarTaxiWeb.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
