fe26b05b978ee90beec4d39c0e936b0e1c25c226
Mariangela <Mariangela@MARIANGELA-PC>
2016-06-29 03:43:41 -0400

    Initial Dashboard

--- SmarTaxiWeb/App_Start/BundleConfig.cs
+++ SmarTaxiWeb/App_Start/BundleConfig.cs
@@ -9,7 +9,29 @@ namespace SmarTaxiWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/js/plugins/jquery/jquery.min.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquerycontinue").Include(
+                        "~/Scripts/js/plugins/jquery/jquery-ui.min.js",
+                        "~/Scripts/js/plugins/bootstrap/bootstrap.min.js",
+                        "~/Scripts/js/plugins/icheck/icheck.min.js",
+                        "~/Scripts/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js",
+                        "~/Scripts/js/plugins/scrolltotop/scrolltopcontrol.js",
+                        "~/Scripts/js/plugins/morris/raphael-min.js",
+                        "~/Scripts/js/plugins/morris/morris.min.js",
+                        "~/Scripts/js/plugins/rickshaw/d3.v3.js",
+                        "~/Scripts/js/plugins/rickshaw/rickshaw.min.js",
+                        "~/Scripts/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js",
+                        "~/Scripts/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js",
+                        "~/Scripts/js/plugins/bootstrap/bootstrap-datepicker.js",
+                        "~/Scripts/js/plugins/owl/owl.carousel.min.js",
+                        "~/Scripts/js/plugins/moment.min.js",
+                        "~/Scripts/js/plugins/daterangepicker/daterangepicker.js",
+                        "~/Scripts/js/plugins.js",
+                        "~/Scripts/js/actions.js",
+                        "~/Scripts/js/demo_dashboard.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,12 +41,8 @@ namespace SmarTaxiWeb
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/theme-default.css",
                       "~/Content/site.css",
                       "~/Content/font-awesome.css"));
         }
