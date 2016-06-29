0ad3fac67a466c9ef6ee658dac9fcae54361375f
Carlos <Carlos@AZOCAR>
2016-06-29 14:39:52 -0400

    Re -Dependencies Refactory

--- SmarTaxiWeb/App_Start/BundleConfig.cs
+++ SmarTaxiWeb/App_Start/BundleConfig.cs
@@ -12,22 +12,10 @@ namespace SmarTaxiWeb
                         "~/Scripts/js/plugins/jquery/jquery.min.js"
                         ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquerycontinue").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jqueries").Include(
                         "~/Scripts/js/plugins/jquery/jquery-ui.min.js",
                         "~/Scripts/js/plugins/bootstrap/bootstrap.min.js",
-                        "~/Scripts/js/plugins/icheck/icheck.min.js",
                         "~/Scripts/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js",
-                        "~/Scripts/js/plugins/scrolltotop/scrolltopcontrol.js",
-                        "~/Scripts/js/plugins/morris/raphael-min.js",
-                        "~/Scripts/js/plugins/morris/morris.min.js",
-                        "~/Scripts/js/plugins/rickshaw/d3.v3.js",
-                        "~/Scripts/js/plugins/rickshaw/rickshaw.min.js",
-                        "~/Scripts/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js",
-                        "~/Scripts/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js",
-                        "~/Scripts/js/plugins/bootstrap/bootstrap-datepicker.js",
-                        "~/Scripts/js/plugins/owl/owl.carousel.min.js",
-                        "~/Scripts/js/plugins/moment.min.js",
-                        "~/Scripts/js/plugins/daterangepicker/daterangepicker.js",
                         "~/Scripts/js/plugins.js",
                         "~/Scripts/js/actions.js",
                         "~/Scripts/js/demo_dashboard.js"
